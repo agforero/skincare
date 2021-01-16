597d15cb08af12a13e7d8816a1598495bd9f6318
Agustin <agforero@gmail.com>
2021-01-16 00:05:46 -0500

    first python commit

---
+++ skincare.py
@@ -0,0 +1,75 @@
+def main():
+    # True if good, False if bad
+    oily = {
+        "Salicylic Acid": True,
+        "Benzoyl Peroxide": True,
+        "Niacinamide": True,
+        "Glycolic Acid": True,
+        "Hyaluronic Acid": True,
+        "Dimethicone": True,
+        "Sodium Hyaluronate": True,
+        "Retinol": True,
+        "L-ascorbic acid (Vitamin C)": True,
+        "Clay": True,
+
+        "SD Alcohol": False,
+        "Denatured Alcohol": False,
+        "Tocopheryl Acetate": False,
+        "Witch Hazel": False,
+        "Sodium C14-16 Olefin Sulfate": False,
+        "Sodium Lauryl Sulfate": False,
+        "Fragrance": False,
+    }
+
+"""
+- Polysorbate 85 
+- Butylene Glycol 
+- Cocamidopropyl Betaine 
+- Urea 
+- Cetearyl Alcohol 
+- Shea Butter 
+- Sodium Hyaluronate 
+- Stearic Acid 
+- Propylene Glycol 
+- Glycerin 
+
+- SD Alcohol 
+- Denatured Alcohol 
+- Isopropyl Alcohol 
+- Witch Hazel 
+- Sodium Laureth Sulfate 
+- Menthol 
+- Sodium Lauryl Sulfate 
+- Fragrance 
+
+
+
+- Glycolic Acid 
+- Panthenol 
+- Sodium Hyaluronate 
+- Retinol 
+- Dimethicone 
+- Hyaluronic Acid 
+
+- SD Alcohol 
+- Denatured Alcohol 
+- Isopropyl Alcohol 
+- Witch Hazel 
+- Sodium Laureth Sulfate 
+- Menthol 
+- Sodium Lauryl Sulfate 
+- Fragrance 
+- Sodium Chloride 
+
+
+
+- Zinc Oxide 
+- Titanium Dioxide 
+- Butylyparaben 
+- 4MBC 
+
+- Oxybenzone 
+- Octocrylene 
+- Butylyparaben 
+- 4MBC 
+"""
